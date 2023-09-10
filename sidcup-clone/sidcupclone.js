ebe1b6d6bda71c0e5c2a0dbe86bec43ccdf94570
joseph babu <josehp304@gmail.com>
2023-09-10 19:50:10 +0530

    sidcup clone 80% done

--- sidcup-clone/sidcupclone.js
+++ sidcup-clone/sidcupclone.js
@@ -1,10 +1,64 @@
 var crsr = document.querySelector(".cursor");
 var crsrb = document.querySelector(".cursor-blur");
 document.addEventListener("mousemove", function (dets) {
-  crsr.style.left = dets.x + "px";
-  crsr.style.top = dets.y + "px";
+  crsr.style.left = dets.x + 10 + "px";
+  crsr.style.top = dets.y + 10 + "px";
   crsrb.style.left = dets.x - 200 + "px";
   crsrb.style.top = dets.y - 200 + "px";
+});
+var navli = document.querySelectorAll(".nav li");
+navli.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    crsr.style.scale = 3;
+    crsr.style.border = "1px solid white";
+    crsr.style.backgroundColor = "transparent";
+  });
+  elem.addEventListener("mouseleave", function () {
+    crsr.style.scale = 1;
+    crsr.style.border = "0px solid white";
+    crsr.style.backgroundColor = "#95c11e";
+  });
+});
+
+var trg = document.querySelectorAll("#ca");
+trg.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    crsr.style.scale = 3;
+    crsr.style.border = "1px solid white";
+    crsr.style.backgroundColor = "transparent";
+  });
+  elem.addEventListener("mouseleave", function () {
+    crsr.style.scale = 1;
+    crsr.style.border = "0px";
+    crsr.style.backgroundColor = "#95c11e";
+  });
+});
+var trgcard = document.querySelectorAll(".card");
+trgcard.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    crsr.style.scale = 3;
+    crsr.style.border = "1px solid white";
+    crsr.style.backgroundColor = "transparent";
+  });
+  elem.addEventListener("mouseleave", function () {
+    crsr.style.scale = 1;
+    crsr.style.border = "0px";
+    crsr.style.backgroundColor = "#95c11e";
+  });
+});
+
+var trgcardw = document.querySelectorAll(".cardw");
+trgcardw.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    crsr.style.scale = 3;
+    crsr.style.border = "1px solid white";
+    crsr.style.backgroundColor = "transparent";
+  });
+  elem.addEventListener("mouseleave", function () {
+    crsr.style.scale = 1;
+    crsr.style.border = "0px";
+    crsr.style.backgroundColor = "#95c11e";
+  });
 });
 
 gsap.to(".nav", {
@@ -55,5 +109,47 @@ gsap.from("#down", {
     trigger: ".quotes",
     scroller: "body",
     scrub: 3,
+  },
+});
+
+gsap.from(".about", {
+  y: 300,
+  scrollTrigger: {
+    trigger: ".about",
+    scroller: "body",
+    scrub: 2,
+  },
+});
+
+gsap.from(".card_container", {
+  x: -200,
+  scrollTrigger: {
+    trigger: ".about",
+    scroller: "body",
+
+    // markers: true,
+    scrub: 3,
+  },
+});
+
+gsap.from(".signup .content3", {
+  x: 400,
+  y: 80,
+  scrollTrigger: {
+    trigger: ".signup",
+    scroller: "body",
+
+    // markers: true,
+    scrub: 3,
+  },
+});
+
+gsap.from(".waiting h1", {
+  y: 30,
+  scrollTrigger: {
+    trigger: ".waiting",
+    scroller: "body",
+    scrub: 3,
+    duration: 2,
   },
 });
