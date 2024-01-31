5f7a515cba3a674978a157591a09a95dee426773
joseph babu <josehp304@gmail.com>
2024-01-31 21:59:26 +0530

    tts done

--- tts/tts.js
+++ tts/tts.js
@@ -3,11 +3,51 @@ const speed = document.querySelector("#speed");
 const play = document.querySelector(".play");
 const pause = document.querySelector(".pause");
 const stop = document.querySelector(".stop");
+const resume = document.querySelector(".resume");
+resume.style.display = "none";
+pause.style.display = "none";
+
 play.addEventListener("click", () => {
-  playtext();
+  playtts();
 });
-function playtext() {
+stop.addEventListener("click", () => {
+  stoptts();
+});
+pause.addEventListener("click", () => {
+  pausetts();
+});
+resume.addEventListener("click", () => {
+  resumetts();
+});
+function playtts() {
   const tts = new SpeechSynthesisUtterance(text.value);
   tts.rate = speed.value || 1;
+  text.disabled = true;
   speechSynthesis.speak(tts);
+  play.style.display = "none";
+  pause.style.display = "";
+  tts.addEventListener("end", () => {
+    text.disabled = false;
+    play.style.display = "";
+    pause.style.display = "none";
+  });
+}
+function stoptts() {
+  text.disabled = false;
+  play.style.display = "";
+  pause.style.display = "none";
+  resume.style.display = "none";
+  speechSynthesis.cancel();
+}
+
+function pausetts() {
+  speechSynthesis.pause();
+  pause.style.display = "none";
+  resume.style.display = "";
+}
+
+function resumetts() {
+  pause.style.display = "";
+  resume.style.display = "none";
+  speechSynthesis.resume();
 }
